7156c28821fd0deb1a70da17a116a753231c7d04
Diego Santos <133829130+diegookys@users.noreply.github.com>
2023-09-04 13:32:04 -0300

    Links beecrowd.c

--- area do circulo.c
+++ area do circulo.c
@@ -1,18 +1,1 @@
-#include <stdio.h>
-
-int main(){
-    
-    // r= raio
-    // a= area
-    // pi
-    
-    double r;
-    double a;
-    double pi=3.14159;
-    
-    scanf ("%lf", &r);
-    a = pi * (r *r);
-    printf ("A=%.4lf\n",a);
-
-    return 0;
-}
+Área do círculo: https://www.beecrowd.com.br/judge/pt/runs/code/35357475
