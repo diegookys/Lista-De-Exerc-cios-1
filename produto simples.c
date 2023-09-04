ff59703c7cb338767de1ecb0505b4bafaf7e1053
Diego Santos <133829130+diegookys@users.noreply.github.com>
2023-09-04 13:54:18 -0300

    Create produto simples.c

---
+++ produto simples.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+    // PRODUTO
+    
+    int a;
+    int b;
+    int PROD;
+    
+    scanf("%d",&a);
+    scanf("%d",&b);
+
+    PROD = a*b;
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
